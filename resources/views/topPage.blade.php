6abf8127c1f2cd0cacf7a4b150e7d430fe2c5626
Shoma700 <ec2-user@ip-172-31-27-225.us-east-2.compute.internal>
2020-09-11 02:27:47 +0000

    first commit

---
+++ resources/views/topPage.blade.php
@@ -0,0 +1,13 @@
+<!DoCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="widt=debice-width, initial-scale=1">
+        
+        <title>maptest</title>
+    </head>
+    <body>
+        <h5>maptest</h5>
+    </body>
+</html>
